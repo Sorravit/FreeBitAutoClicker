6344bdd4b9fbb656c69f6824538d7803428a30e4
Big <sorravit.bunjongpean@refinitiv.com>
2021-05-20 11:57:36 +0700

    Use alarms to print time every two min

--- background.js
+++ background.js
@@ -4,3 +4,10 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+console.log("HELLO")
+console.log(chrome.alarms.create("SimpleLog", {periodInMinutes: 2}));
+chrome.alarms.onAlarm.addListener(function (alarm){
+  console.log(alarm.name);
+  const now = new Date();
+  console.log(now.toUTCString());
+})
