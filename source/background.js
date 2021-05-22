8891d9dc563bbb517cb7146c549be519274721fa
Big <sorravit.bunjongpean@refinitiv.com>
2021-05-22 16:27:44 +0700

    Handle multiple alarm

--- source/background.js
+++ source/background.js
@@ -5,19 +5,34 @@ chrome.runtime.onInstalled.addListener(() => {
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
 console.log("HELLO")
-chrome.alarms.create("SimpleLog", {periodInMinutes: 2})
+chrome.alarms.create("SimpleLog", {periodInMinutes: 10 / 60})
 chrome.alarms.onAlarm.addListener(function (alarm) {
-  console.log(alarm.name);
-  const now = new Date();
-  console.log(now.toUTCString());
+  console.log(alarm.name)
+  if (alarm.name === "SimpleLog") {
+    console.log("Logging because it's a simple log alarm")
+    const now = new Date();
+    console.log(now.toUTCString());
+  } else if (alarm.name === "ClickButton") {
+    chrome.storage.sync.get("tabID", ({tabID}) => {
+      chrome.scripting.executeScript({
+        target: {tabId: tabID},
+        function: clickButton,
+      });
+    });
+
+  }
 })
+
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   console.log("Get message '", message, "' from tab id ", sender.tab.id);
   if (message === 'tabID') {
     console.log("Getting tabID from tab")
+    const tabID = sender.tab.id
+    chrome.storage.sync.set({tabID});
+    chrome.alarms.create("ClickButton", {periodInMinutes: 10 / 60})
     chrome.scripting.executeScript({
-      target: {tabId: sender.tab.id},
+      target: {tabId: tabID},
       function: clickButton,
     });
     sendResponse("TabId received")
@@ -31,4 +46,5 @@ function callFromBackground() {
 function clickButton() {
   const button = document.getElementById("clickMe");
   button.click();
+
 }
