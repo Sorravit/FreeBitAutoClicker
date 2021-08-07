8c047f354636f3d102ca2705801cd9f5a1652f1d
Big <sorravit.bunjongpean@refinitiv.com>
2021-08-07 20:52:47 +0700

    Auto click on 100 time reward points multiplier once previous reward ran out

--- source/popup.js
+++ source/popup.js
@@ -1,5 +1,7 @@
 let startAutoClick = document.getElementById("startAutoClick");
 let stopAutoClick = document.getElementById("stopAutoClick");
+let startAutoReward = document.getElementById("startAutoReward");
+let stopAutoReward = document.getElementById("stopAutoReward");
 
 startAutoClick.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
@@ -17,6 +19,23 @@ stopAutoClick.addEventListener("click", async () => {
   });
 });
 
+startAutoReward.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+  chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    function: sendStartAutoRewardCommand,
+  });
+});
+
+stopAutoReward.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+  chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    function: sendDeleteAutoRewardAlarmCommand,
+  });
+});
+
+
 function sendDeleteAlarmCommand() {
   console.log("Sending Delete Alarm Command")
   chrome.runtime.sendMessage("StopAutoClick", (response => {
@@ -27,6 +46,20 @@ function sendDeleteAlarmCommand() {
 function sendStartAutoClickCommand() {
   console.log("Sending tabID to start Auto Click")
   chrome.runtime.sendMessage("StartAutoClick", (response => {
+    console.log("Response :", response)
+  }))
+}
+
+function sendDeleteAutoRewardAlarmCommand() {
+  console.log("Sending Delete Alarm Command")
+  chrome.runtime.sendMessage("StopAutoReward", (response => {
+    console.log("Response :", response)
+  }))
+}
+
+function sendStartAutoRewardCommand() {
+  console.log("Sending tabID to start Auto Click")
+  chrome.runtime.sendMessage("StartAutoReward", (response => {
     console.log("Response :", response)
   }))
 }
